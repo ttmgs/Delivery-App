0b0f00138e9d6b28439b22023ed75c7df20c93d4
Magdi Tiea <magditiea@gmail.com>
2021-04-17 12:18:47 -0400

    fix

--- server.js
+++ server.js
@@ -2,17 +2,19 @@ var express = require("express");
 var mongoose = require("mongoose");
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/chappodelivery", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/serviceWorkerDemo", {
   useNewUrlParser: true
 });
 
 app.listen(PORT, function() {
-  console.log('Now listening on http://localhost:' + PORT);
+  console.log(`Now listening on port: ${PORT}`);
 });
